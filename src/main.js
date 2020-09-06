6d1e97e902eda72a0811f0c0cc7bad59922475c6
Andrew <andersen7298@gmail.com>
2020-09-06 21:41:45 +0300

    Добавлена заглушка при отсутствии задач и отрефакторен main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import LoadMoreBtnView from './view/loadMoreBtn.js';
 import FilterView from './view/filter.js';
 import TaskView from './view/task.js';
 import TaskEditView from './view/taskEdit.js';
+import NoTaskView from './view/noTask.js';
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
 import {render, RENDER_POSITION} from "./utils.js";
@@ -53,40 +54,53 @@ const tasksData = new Array(TASKS_COUNT).fill().map(generateTask);
 const filtersData = generateFilter(tasksData);
 
 
-const siteMainNode = document.querySelector(`.main`);
+const renderBoard = (boardContainer, boardTasks) => {
+  const boardComponent = new BoardView();
 
+  const siteTasksBoardNode = boardComponent.getElement().querySelector(`.board__tasks`);
+
+  let renderTemplateedTaskCount = TASKS_COUNT_PER_STEP;
+
+  render(boardContainer, boardComponent.getElement(), RENDER_POSITION.beforeend);
+
+  if (tasksData.every((task) => task.isArchive)) {
+    render(boardComponent.getElement(), new NoTaskView().getElement(), RENDER_POSITION.afterbegin);
+    return;
+  }
+
+  render(boardComponent.getElement(), new SortView().getElement(), RENDER_POSITION.afterbegin);
+
+  boardTasks
+    .slice(0, Math.min(boardTasks.length, TASKS_COUNT_PER_STEP))
+    .forEach((boardTask) => renderTask(siteTasksBoardNode, boardTask));
+
+  if (boardTasks.length > TASKS_COUNT_PER_STEP) {
+
+    const loadMoreBtnComponent = new LoadMoreBtnView();
+
+    render(boardComponent.getElement(), loadMoreBtnComponent.getElement(), RENDER_POSITION.beforeend);
+
+    loadMoreBtnComponent.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      boardTasks
+        .slice(renderTemplateedTaskCount, renderTemplateedTaskCount + TASKS_COUNT_PER_STEP)
+        .forEach((task) => renderTask(siteTasksBoardNode, task));
+
+      renderTemplateedTaskCount += TASKS_COUNT_PER_STEP;
+
+      if (renderTemplateedTaskCount >= boardTasks.length) {
+        loadMoreBtnComponent.getElement().remove();
+      }
+    });
+  }
+};
+
+const siteMainNode = document.querySelector(`.main`);
 const siteHeaderNode = siteMainNode.querySelector(`.main__control`);
 
 render(siteHeaderNode, new MenuView().getElement(), RENDER_POSITION.beforeend);
 render(siteMainNode, new FilterView(filtersData).getElement(), RENDER_POSITION.beforeend);
-render(siteMainNode, new BoardView().getElement(), RENDER_POSITION.beforeend);
 
-const siteBoardNode = siteMainNode.querySelector(`.board`);
-const siteTasksBoardNode = siteBoardNode.querySelector(`.board__tasks`);
+renderBoard(document.querySelector(`main`), tasksData);
 
-render(siteBoardNode, new SortView().getElement(), RENDER_POSITION.afterbegin);
 
-for (let i = 0; i < Math.min(tasksData.length, TASKS_COUNT_PER_STEP); i++) {
-  renderTask(siteTasksBoardNode, tasksData[i]);
-}
-
-if (tasksData.length > TASKS_COUNT_PER_STEP) {
-  let renderTemplateedTaskCount = TASKS_COUNT_PER_STEP;
-
-  render(siteBoardNode, new LoadMoreBtnView().getElement(), RENDER_POSITION.beforeend);
-
-  const loadMoreButton = siteBoardNode.querySelector(`.load-more`);
-
-  loadMoreButton.addEventListener(`click`, (evt) => {
-    evt.preventDefault();
-    tasksData
-      .slice(renderTemplateedTaskCount, renderTemplateedTaskCount + TASKS_COUNT_PER_STEP)
-      .forEach((task) => renderTask(siteTasksBoardNode, task));
-
-    renderTemplateedTaskCount += TASKS_COUNT_PER_STEP;
-
-    if (renderTemplateedTaskCount >= tasksData.length) {
-      loadMoreButton.remove();
-    }
-  });
-}
