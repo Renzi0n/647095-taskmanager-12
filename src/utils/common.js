c79638c2b218cab89f00512c8162be0cbaafaceb
Andrew <andersen7298@gmail.com>
2020-09-12 20:48:23 +0300

    Настроен датабиндинг

--- src/utils/common.js
+++ src/utils/common.js
@@ -14,3 +14,17 @@ export const getRandomElements = (elements) => {
 export const getRandomBooleanValue = () => {
   return Boolean(getRandomInteger(0, 1));
 };
+
+export const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1)
+  ];
+};
